69fb5ca86b68fb4cd87542dce92e2b6b1edfb7e9
sandhya <goswami.sa@northeastern.edu>
2021-08-19 18:09:23 -0400

    adding plain files

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,9 @@
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : yeah faced problem calculating Time n Space complexity. looking for more efficient approach.
+
+
+// Your code here along with comments explaining your approach
+
+// Design Queue Using Stack
